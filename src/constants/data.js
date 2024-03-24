2f53383c605681728000fe063bc9ab886bf76303
Den <91518057+kark4rasev@users.noreply.github.com>
2024-03-24 21:23:53 +0300

    Create data.js

---
+++ src/constants/data.js
@@ -0,0 +1,140 @@
+import images from "./images";
+
+const services = [
+    {
+        image: `${images.services_img_1}`,
+        title: "Web Design",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        image: `${images.services_img_2}`,
+        title: "Branding",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        image: `${images.services_img_3}`,
+        title: "UX Design",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        image: `${images.services_img_4}`,
+        title: "Logo Create",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        image: `${images.services_img_5}`,
+        title: "Illustration",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+    {
+        image: `${images.services_img_6}`,
+        title: "Graphics",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam"
+    },
+
+];
+
+const works = [
+    {
+        image:`${images.work_img_1}`
+    },
+    {
+        image:`${images.work_img_2}`
+    },
+    {
+        image:`${images.work_img_3}`
+    },
+    {
+        image:`${images.work_img_4}`
+    },
+    {
+        image:`${images.work_img_5}`
+    },
+    {
+        image:`${images.work_img_6}`
+    },
+
+];
+
+const work_process = [
+    {
+        title:"Sketch",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    },
+    {
+        title:"Design",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    },
+    {
+        title:"Develop",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    },
+    {
+        title:"End-Product",
+        paragraph: "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+    },
+];
+
+const about_stats = [
+    {
+        image:`${images.stat_img_1}`,
+        value: "1775",
+        title: "Clirents"
+    },
+    {
+        image:`${images.stat_img_2}`,
+        value: "2416",
+        title: "Projects"
+    },
+    {
+        image:`${images.stat_img_3}`,
+        value: "4287",
+        title: "Working Hours"
+    },
+    {
+        image:`${images.stat_img_4}`,
+        value: "287",
+        title: "Awards"
+    },
+];
+
+const testimonials = [
+    {
+        name:"Lelia Merritt",
+        post: "Graphic Designer",
+        paragraph: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…”"
+    },
+    {
+        name:"John Huston",
+        post: "Web Developer",
+        paragraph: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…”"
+    },
+    {
+        name:"Jeniffer Green",
+        post: "Marketing manager",
+        paragraph: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit…” “There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain…”"
+    },
+];
+
+const logos = [
+    {
+        images: `${images.logo_img_1}`
+    },
+    {
+        images: `${images.logo_img_2}`
+    },
+    {
+        images: `${images.logo_img_3}`
+    },
+    {
+        images: `${images.logo_img_4}`
+    },
+    {
+        images: `${images.logo_img_5}`
+    },
+    {
+        images: `${images.logo_img_6}`
+    },
+];
+
+export {services,works,work_process,about_stats,testimonials,logos};
